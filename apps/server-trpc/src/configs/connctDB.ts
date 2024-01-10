e68eb199e7b93136dcada51bff13a2605ca1d12e
ShmuelMachpud <sm0524432584@gmail.com>
2024-01-10 17:09:25 +0200

    נשאר לחבר לקוח לשרת

--- apps/server-trpc/src/configs/connctDB.ts
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from 'sequelize'
-
-// export const sequelize = new Sequelize('postgres://xztowbjg:L0aTWeyzd9EHkZ96URQtlsQOi03kSn5e@tyke.db.elephantsql.com/xztowbjg');
-
-export const sequelize = new Sequelize('E.M.S', 'postgres','206830895', {
-    host:'localhost',
-    dialect: 'postgres'
-})
-
--- apps/server-trpc/src/trpc/queryTrpc.ts
+++ apps/server-trpc/src/trpc/queryTrpc.ts
@@ -1,23 +1,30 @@
-import { log } from 'console';
-// import {employee} from '../models/employee'
-import {sequelize} from '../configs/connctDB'
+import {Employee} from '../models/employee'
+// import {sequelize} from '../configs/connctDB'
 import {router, publicProcedure} from './trpc'
-import { z } from "zod";
+// import { z } from "zod";
 
-export const appRouter = router({
-    allEmployees:publicProcedure.query(async () => {
-        const data = await sequelize.models.employee.findAll()
-        .then((employees) => {return employees})
-        // console.log(data);
-        return data
-    }),
-    employeeById: publicProcedure
-    .input(z.string())
-    .query(async(firstName) => {
-        const {input} = firstName;
-        const employeeId = await sequelize.models.employee.findByPk(input)
-        return employeeId
+const appRouter = router({
+    allEmployees: publicProcedure.query(async () =>{
+        const allEmployees = await Employee.findAll();
+        return allEmployees
     })
 })
 
-export type AppRouter = typeof appRouter
+export type AppRouter = typeof appRouter;
+// export const appRouter = router({
+//     allEmployees:publicProcedure.query(async () => {
+//         const data = await sequelize.models.employee.findAll()
+//         .then((employees) => {return employees})
+//         // console.log(data);
+//         return data
+//     }),
+//     employeeById: publicProcedure
+//     .input(z.string())
+//     .query(async(firstName) => {
+//         const {input} = firstName;
+//         const employeeId = await sequelize.models.employee.findByPk(input)
+//         return employeeId
+//     })
+// })
+
+// export type AppRouter = typeof appRouter
