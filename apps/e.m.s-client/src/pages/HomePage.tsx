a0e7734b4243dc89a8022a7d026e2ef8d0858d4e
ShmuelMachpud <sm0524432584@gmail.com>
2024-01-14 17:21:57 +0200

    תחילת עיצוב לקוח

---
+++ apps/e.m.s-client/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BulkRecordError } from 'sequelize'
+import TableAllEmployees from '../components/home/TableAllEmployees'
+
+
+
+
+
+export default function HomePage() {
+
+ 
+  return (
+    <TableAllEmployees/>
+  )
+}
