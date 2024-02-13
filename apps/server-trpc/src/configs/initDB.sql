13b9c3e5af26bbead91608dce43ab595833ba0b6
Shmuel <sm0524432584@gmail.com>
2024-02-13 12:36:11 +0200

    updata initDB

--- apps/server-trpc/src/configs/initDB.sql
+++ apps/server-trpc/src/configs/initDB.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS employees(
-	Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	Id UUID  DEFAULT gen_random_uuid(),
 	first_name VARCHAR(21) NOT NULL,
 	last_name  VARCHAR(21) NOT NULL,
-	email VARCHAR(21) NOT NULL,
-	phone_number VARCHAR(21) NOT NULL,
+	email   VARCHAR(700) PRIMARY KEY ,
+	phone_number INT NOT NULL,
 	branch_number INT NOT NULL,
 	role VARCHAR(21) NOT NULL
 )
@@ -16,7 +16,7 @@ INSERT INTO employees (first_name, last_name, email, phone_number, branch_number
 
 CREATE TABLE IF NOT EXISTS branches(
 	branch_number INT PRIMARY KEY NOT NULL,
-	branch_name  VARCHAR(21) NOT NULL,
+	branch_name  VARCHAR(50) NOT NULL,
 	branch_phone INT NOT NULL,
 	branch_address VARCHAR(500) NOT NULL
 )
